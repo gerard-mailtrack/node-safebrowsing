e5575bd3091beb6c368f92e76237d62445300d2d
Gerard Ruiz <gerard@mailtrack.io>
2016-05-10 14:40:24 +0200

    Removes transpilation
    We're using node v4

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,42 +1,22 @@
 var path = require('path');
 var gulp = require('gulp');
 var jshint = require('gulp-jshint');
-var jstransform = require('gulp-jstransform');
-var regenerator = require('gulp-regenerator');
 var gutil = require('gulp-util');
 var mocha = require('gulp-mocha');
 var instanbul = require('gulp-istanbul');
 var coveralls = require('gulp-coveralls');
 
-gulp.task('build-js', function() {
-  return gulp.src('src/**/*.js')
-    .pipe(jstransform())
-    .pipe(regenerator())
-    .pipe(gulp.dest('lib'));
-});
-
-gulp.task('build-proto', function() {
-  return gulp.src('src/**/*.proto')
-    .pipe(gulp.dest('lib'));
-});
-
-gulp.task('build', ['build-js', 'build-proto']);
-
 gulp.task('lint', function() {
   return gulp.src('src/**/*.js')
     .pipe(jshint())
     .pipe(jshint.reporter('default'));
-})
+});
 
 gulp.task('test', ['lint', 'build'], function(done) {
-  gulp.src(['lib/**/*.js', '!lib/**/*-test.js'])
-    .pipe(instanbul())
-    .on('finish', function() {
-      gulp.src('lib/**/__tests__/*.js', {read: false})
-        .pipe(mocha({reporter: 'spec'}))
-        .pipe(instanbul.writeReports())
-        .on('end', done);
-    });
+  gulp.src('src/**/__tests__/*.js', {read: false})
+    .pipe(mocha({reporter: 'spec'}))
+    .pipe(instanbul.writeReports())
+    .on('end', done);
 });
 
 gulp.task('submit-coverage', function() {
@@ -48,4 +28,4 @@ gulp.task('watch', function() {
   gulp.watch('src/**/*', ['test']);
 });
 
-gulp.task('default', ['build']);
+gulp.task('default', ['test']);
